9ac197aa7d9449e3e173b31b93115a77089f4c4d
alvess-dev <luis.rodrigues.dev@gmail.com>
2025-11-25 20:12:22 -0300

    chore: add backend dependencies on gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,12 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.5")
     implementation("androidx.navigation:navigation-compose:2.8.4")
     implementation("androidx.compose.animation:animation:1.7.5")
-
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
 }
